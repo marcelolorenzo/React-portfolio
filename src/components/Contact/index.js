a5410867b493f485cd109b363872dc1fb6766283
Marcelo Lorenzo <marcelolorenzo@MacBook-Air-de-Marcelo.local>
2022-09-19 21:25:59 -0300

    subindo react-portfolio

---
+++ src/components/Contact/index.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                        />
+                        idx={15}
+                    </h1>
+                    <p>
+                        I am interested in freelance opportunities - especially on ambitious
+                        or large projects. However, if you have any other requests or
+                        questions, don't hesitate to contact me using below form either.
+                    </p>
+                </div>
+            </div>
+            <Loader type='pacman' />
+        </>
+    )
+}
+
+export default Contact
